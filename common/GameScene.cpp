9283568da986efc4ced7de68dda3088b9fab4176
Ulysse Darmet <darmet.ulysse@gmail.com>
2020-01-12 18:56:07 +0100

    Display background image

--- common/GameScene.cpp
+++ common/GameScene.cpp
@@ -6,6 +6,9 @@
 using namespace cocos2d;
 
 
+static constexpr char BACKGROUND_FILE_NAME[] = "res/chalkboard.jpg";
+
+
 Scene* GameScene::createScene()
 {
     return GameScene::create();
@@ -18,6 +21,17 @@ bool GameScene::init()
     {
         return false;
     }
+
+    // display background image
+    auto winSize = Director::getInstance()->getWinSize();
+    auto backgroundSprite = Sprite::create();
+    backgroundSprite->initWithFile(BACKGROUND_FILE_NAME);
+    backgroundSprite->setAnchorPoint(Vec2(0, 0));
+    backgroundSprite->setPosition(Vec2(0, 0));
+    auto backgroundSize = backgroundSprite->getContentSize();
+    backgroundSprite->setScale(std::max(winSize.width / backgroundSize.width, winSize.height / backgroundSize.height));
+    addChild(backgroundSprite);
+
     return true;
 }
 
