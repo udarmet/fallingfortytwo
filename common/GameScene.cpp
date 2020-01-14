90d20c9c92b330cd3cae2eeff5838177270002c6
Ulysse Darmet <darmet.ulysse@gmail.com>
2020-01-14 21:17:28 +0100

    Add some text

--- common/GameScene.cpp
+++ common/GameScene.cpp
@@ -7,12 +7,22 @@ using namespace cocos2d;
 
 
 static const char BACKGROUND_FILE_NAME[] = "res/chalkboard.jpg";
+
+static const char TITLE_LABEL_TEXT[] = "FALLING\nFORTY-TWO";
+static const char TITLE_LABEL_FONT_FILE_NAME[] = "fonts/KG Second Chances Sketch.ttf";
+static const int TITLE_LABEL_FONT_SIZE = 40;
+
 static const char CIRCLE_SPRITESHEET_FILE_NAME[] = "res/chalk-circle.plist";
 static const char CIRCLE_SPRITE_NAME[] = "chalk-circle/00";
 static const Size CIRCLE_SPRITE_SIZE = {64, 64};
+
 static const char LINE_SPRITESHEET_FILE_NAME[] = "res/chalk-line.plist";
 static const char LINE_SPRITE_NAME[] = "chalk-line/00";
 static const Size LINE_SPRITE_SIZE = {320, 320.f/1419*91};
+
+static const char SOME_TEXT_LABEL_TEXT[] = "You're gonna enjoy\nthis new version\nso much!";
+static const char SOME_TEXT_FONT_FILE_NAME[] = "fonts/DK Cool Crayon.ttf";
+static const int SOME_TEXT_FONT_SIZE = 24;
 
 
 Scene* GameScene::createScene()
@@ -38,6 +48,13 @@ bool GameScene::init()
     backgroundSprite->setScale(std::max(winSize.width / backgroundSize.width, winSize.height / backgroundSize.height));
     addChild(backgroundSprite);
 
+    // display title
+    auto titleLabel = Label::createWithTTF(TITLE_LABEL_TEXT, TITLE_LABEL_FONT_FILE_NAME, TITLE_LABEL_FONT_SIZE);
+    titleLabel->setAlignment(TextHAlignment::CENTER);
+    titleLabel->setAnchorPoint(Vec2::ANCHOR_MIDDLE_TOP);
+    titleLabel->setPosition(Vec2(winSize.width / 2, winSize.height));
+    addChild(titleLabel);
+
     // display circle
     SpriteFrameCache::getInstance()->addSpriteFramesWithFile(CIRCLE_SPRITESHEET_FILE_NAME);
     auto circleSprite = Sprite::createWithSpriteFrameName(CIRCLE_SPRITE_NAME);
@@ -51,6 +68,13 @@ bool GameScene::init()
     lineSprite->setPosition(winSize / 2);
     lineSprite->setContentSize(LINE_SPRITE_SIZE);
     addChild(lineSprite);
+
+    // display some text
+    auto someTextLabel = Label::createWithTTF(SOME_TEXT_LABEL_TEXT, SOME_TEXT_FONT_FILE_NAME, SOME_TEXT_FONT_SIZE);
+    someTextLabel->setAlignment(TextHAlignment::CENTER);
+    someTextLabel->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    someTextLabel->setPosition(Vec2(winSize.width / 2, winSize.height / 4));
+    addChild(someTextLabel);
 
     return true;
 }
